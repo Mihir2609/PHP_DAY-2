a60a0842ed58de7921f9312bdd1e4036ecc51e5c
Mihir2609 <84737914+Mihir2609@users.noreply.github.com>
2021-05-26 23:10:42 +0530

    V2
    Perform php code like variable,if statement, switch case, palindrom

---
+++ PHP_DAY2/index1.php
@@ -0,0 +1,12 @@
+<?php
+echo "welcome to php";
+echo "<h1> i am H1 in PHP</h1>";
+?>
+<html>
+     <body>
+       welcome to HTML code
+       <?php
+       echo "i am PHP Inside HTML"; 
+       ?>
+   </body>
+ </html> 
